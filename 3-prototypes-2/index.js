74c656eb1487ca01bf9aef89f78ef40ffe704a02
fernando henrique <fernandomk6@gmail.com>
2022-06-15 14:47:50 -0300

    mais sobre prototypes

--- 3-prototypes-2/index.js
+++ 3-prototypes-2/index.js
@@ -13,26 +13,41 @@ class Child extends Father {
 
 const bob = new Child();
 
-console.log(Object.getPrototypeOf(bob) === Child.prototype);
-console.log(Object.getPrototypeOf(Child.prototype) === Father.prototype);
-console.log(Object.getPrototypeOf(Father.prototype) === Object.prototype);
-console.log(Object.getPrototypeOf(Object.prototype) === null);
+// Prototype de function não tem name
+// Function.prototype.name 
+// Function.prototype.construcotr.name retorna Function como string
 
-console.log(Object.getPrototypeOf(Child) === Father);
-console.log(Object.getPrototypeOf(Father) === Function.prototype);
-console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
-console.log(Object.getPrototypeOf(Object.prototype) === null);
+// Todos os outros prototypes tem names?
+// Toda declaração de função tem uma propriedade name
+// Objetos não tem propriedade name por dafault
 
-console.log(Object.getPrototypeOf(Function) === Function.prototype);
-console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
-console.log(Object.getPrototypeOf(Object.prototype) === null);
+// Function.prototype = uma função anonima
+// prototypes podem ser funções????
 
-// Entender bem isso aqui primeiro
-console.log(Child === Child.prototype);
-console.log(Child);
-console.log(Child.prototype);
+// Pq proto de Child tem name e proto de Father não tem name ??
+console.log(Object.getPrototypeOf(Father).constructor.name);
+console.log(Object.getPrototypeOf(Child).constructor.name);
 
-console.log('---');
+// Object.getPrototypeOf(Child) -> Father
+// Father.constructor === Function
+// Father.prototype === Father
+
+// pq isso não funciona
+console.log(Object.getPrototypeOf(Child) === Father.prototype); // False
+
+// console.log(Object.getPrototypeOf(bob) === Child.prototype);
+// console.log(Object.getPrototypeOf(Child.prototype) === Father.prototype);
+// console.log(Object.getPrototypeOf(Father.prototype) === Object.prototype);
+// console.log(Object.getPrototypeOf(Object.prototype) === null);
+
+// console.log(Object.getPrototypeOf(Child) === Father);
+// console.log(Object.getPrototypeOf(Father) === Function.prototype);
+// console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
+// console.log(Object.getPrototypeOf(Object.prototype) === null);
+
+// console.log(Object.getPrototypeOf(Function) === Function.prototype);
+// console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
+// console.log(Object.getPrototypeOf(Object.prototype) === null);
 
 class Foo {
   constructor() {
@@ -50,14 +65,53 @@ const bar = new Bar();
 const foo = new Foo();
 
 // Lembre-se, prototype é sempre um objeto
-// Objectos não tem prototype
+// Objetos não tem prototype
+// Objetos therdam um prototype (.__proto__ ou Object.getPrototypeOf(Object))
 // Apenas funções possuem prototypes
 
-console.log(typeof Bar); // function
-console.log(typeof Bar.prototype); // object
-console.log(Bar.prototype); // Existe. prototype é sempre um objeto
-console.log(Bar.prototype.prototype); // Não existe. objeto não tem prototype
+
+// console.log(typeof Bar); // function
+// console.log(typeof Bar.prototype); // object
+// console.log(Object.getPrototypeOf(Bar) === Function.prototype);
+
+// verificando a função construtora de alguem
+// let alguem = {};
+
+// verifico o nome da função construtora usada para criar um objeto
+// console.log(Object.getPrototypeOf(alguem).constructor.name); // Object
+
+// depois é so comparar com o prototype do constructor encontrado
+// console.log(Object.getPrototypeOf(alguem) === Object.prototype); // true
 
 
+// prototype sempre vai ser um Objeto
 
+// typeof any.prototype
 
+// criador -> constructor que cria um objeto
+// criado -> instancia é o objeto criado pelo constructor
+
+// apenas o criador possui um prototipo (modelo a ser seguido) pois eu sou 
+// criador, preciso de um modelo para minhas crias
+
+// o que é criado não tem um prototipo
+// ele tem um link ao prototipo, mas não pode alterar o prototipo do qual
+// ele foi criado
+
+// apenas funções tem um prototype
+// prototype é uma propriedade de uma função
+// apenas quem constroi tem prototype
+// apenas funções constroem
+// quem recebe não possui prototype
+
+// typeof any // function
+
+// O que é construido herda o prototypo da função
+
+// apenas funções possuem prototype
+// objeto não tem prototype
+
+// objeto e função são as mesmas coisas
+// não
+
+// como assim objeto não tem prototipo? pelo que eu vi ele herda de alguem
