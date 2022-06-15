b2f296159537881a7c263ad09b40b83da0f42dae
fernando henrique <fernandomk6@gmail.com>
2022-06-15 07:25:20 -0300

    adicionado algumas informações importantes sobre prototypes

--- 3-prototypes-2/index.js
+++ 3-prototypes-2/index.js
@@ -1,56 +1,63 @@
-// class Father {
-//   constructor() {
-//     this.father = true;
-//   }
-// }
-
-// class Child extends Father {
-//   constructor() {
-//     super()
-//     this.child = true;
-//   }
-// }
-
-// const bob = new Child();
-
-// console.log(Object.getPrototypeOf(bob) === Child.prototype);
-// console.log(Object.getPrototypeOf(Child.prototype) === Father.prototype);
-// console.log(Object.getPrototypeOf(Father.prototype) === Object.prototype);
-// console.log(Object.getPrototypeOf(Object.prototype) === null);
-
-// console.log(Object.getPrototypeOf(Child) === Father);
-// console.log(Object.getPrototypeOf(Father) === Function.prototype);
-// console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
-// console.log(Object.getPrototypeOf(Object.prototype) === null);
-
-// console.log(Object.getPrototypeOf(Function) === Function.prototype);
-// console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
-// console.log(Object.getPrototypeOf(Object.prototype) === null);
-
-// // Entender bem isso aqui primeiro
-// console.log(Child === Child.prototype);
-// console.log(Child);
-// console.log(Child.prototype);
-
-// ---
-
 class Father {
   constructor() {
     this.father = true;
   }
 }
 
-class Child{
+class Child extends Father {
   constructor() {
+    super()
     this.child = true;
   }
 }
 
-// Lembre-se, prototype é sempre um objeto
-console.log(typeof Child); // function
-console.log(typeof Child.prototype); // object
-console.log(Child.prototype); // não existe
-console.log(Child.prototype.prototype); // não existe
+const bob = new Child();
 
+console.log(Object.getPrototypeOf(bob) === Child.prototype);
+console.log(Object.getPrototypeOf(Child.prototype) === Father.prototype);
+console.log(Object.getPrototypeOf(Father.prototype) === Object.prototype);
+console.log(Object.getPrototypeOf(Object.prototype) === null);
+
+console.log(Object.getPrototypeOf(Child) === Father);
+console.log(Object.getPrototypeOf(Father) === Function.prototype);
+console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
+console.log(Object.getPrototypeOf(Object.prototype) === null);
+
+console.log(Object.getPrototypeOf(Function) === Function.prototype);
+console.log(Object.getPrototypeOf(Function.prototype) === Object.prototype);
+console.log(Object.getPrototypeOf(Object.prototype) === null);
+
+// Entender bem isso aqui primeiro
+console.log(Child === Child.prototype);
+console.log(Child);
+console.log(Child.prototype);
+
+console.log('---');
+
+class Foo {
+  constructor() {
+    this.foo = true;
+  }
+}
+
+class Bar {
+  constructor() {
+    this.bar = true;
+  }
+}
+
+const bar = new Bar();
+const foo = new Foo();
+
+// Lembre-se, prototype é sempre um objeto
 // Objectos não tem prototype
 // Apenas funções possuem prototypes
+
+console.log(typeof Bar); // function
+console.log(typeof Bar.prototype); // object
+console.log(Bar.prototype); // Existe. prototype é sempre um objeto
+console.log(Bar.prototype.prototype); // Não existe. objeto não tem prototype
+
+
+
+
