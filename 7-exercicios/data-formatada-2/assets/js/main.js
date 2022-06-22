236a323c8da94957d620910996171a91bfa0195d
fernando henrique <fernandomk6@gmail.com>
2022-06-22 20:23:22 -0300

    criado exercicio data formatada 2, usando o metodo toLocale string para formatar

--- 7-exercicios/data-formatada-2/assets/js/main.js
+++ 7-exercicios/data-formatada-2/assets/js/main.js
@@ -2,23 +2,51 @@ const dateContainer = document.querySelector('.date');
 const timeContainer = document.querySelector('.time');
 
 function formatDate(date) {
+  const daysWeek= [
+    'domingo',
+    'segunda-feira',
+    'terça-feira',
+    'quarta-feira',
+    'quita-feira',
+    'sexta-feira',
+    'sábado'
+  ];
+  
+  const monthNames = [
+    'janeiro',
+    'fevereiro',
+    'março',
+    'abril',
+    'maio',
+    'junho',
+    'julho',
+    'agosto',
+    'setembro',
+    'outubro',
+    'novembro',
+    'dezembro'
+  ];
+
+  const dayWeek = date.getDay();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
+  const formatedDayWeek = daysWeek[dayWeek];
+  const formatedDay = day < 10 ? `0${day}` : day;
+  const formatedMonth = monthNames[month];
   const formatedHours = hours < 10 ? `0${hours}` : hours;
   const formatedMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds;
-  
-  const formatedDate = date.toLocaleDateString('pt-BR', {
-    year: 'numeric',
-    month: ('long' || 'short' || 'numeric'),
-    weekday: ('long' || 'short'),
-    day: 'numeric',
-  });
+
+  const formatedDate =
+    `${formatedDayWeek}, ${formatedDay} de ${formatedMonth} de ${year}`;
   const formatedTime = `${formatedHours}:${formatedMinutes}:${formatedSeconds}`;
 
-  return [formatedDate, formatedTime];
+ return [formatedDate, formatedTime];
 }
 
 function renderDate() {
