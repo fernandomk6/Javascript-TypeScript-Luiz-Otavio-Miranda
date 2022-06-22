d0ad2deac7b4e84e520bb9e2af0c483cf6c9afd3
fernando henrique <fernandomk6@gmail.com>
2022-06-22 17:40:18 -0300

    concluido exercicio de formatação de data

--- 7-exercicios/data-formatada/assets/js/main.js
+++ 7-exercicios/data-formatada/assets/js/main.js
@@ -1,12 +1,69 @@
-const container = document.querySelector('.container');
-const date = new Date();
+const dateContainer = document.querySelector('.date');
+const timeContainer = document.querySelector('.time');
+
 
 function formatDate(date) {
- return date.toString();
+  const daysWeek= [
+    'domingo',
+    'segunda-feira',
+    'terça-feira',
+    'quarta-feira',
+    'quita-feira',
+    'sexta-feira',
+    'sábado'
+  ];
+  
+  const monthNames = [
+    'janeiro',
+    'fevereiro',
+    'março',
+    'abril',
+    'maio',
+    'junho',
+    'julho',
+    'agosto',
+    'setembro',
+    'outubro',
+    'novembro',
+    'dezembro'
+  ];
+
+  const dayWeek = date.getDay();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  const formatedDayWeek = daysWeek[dayWeek];
+  const formatedDay = day < 10 ? `0${day}` : day;
+  const formatedMonth = monthNames[month];
+  const formatedHours = hours < 10 ? `0${hours}` : hours;
+  const formatedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const formatedSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  const formatedDate =
+    `${formatedDayWeek} ${formatedDay} de ${formatedMonth} de ${year}`;
+  const formatedTime = `${formatedHours}:${formatedMinutes}:${formatedSeconds}`;
+
+ return [formatedDate, formatedTime];
 }
 
-function renderDate(formatedDate) {
-  container.innerHTML = formatedDate;
+function renderDate() {
+  const date = new Date()
+  const [formatedDate, formatedTime] = formatDate(date);
+
+  dateContainer.innerHTML = formatedDate;
+  timeContainer.innerHTML = formatedTime;
+
+  console.log(date);
 }
 
-renderDate(formatDate(date));
+function init() {
+  setInterval(renderDate, 1000);
+}
+
+init();
+
+
