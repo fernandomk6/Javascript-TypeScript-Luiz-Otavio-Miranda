d4ca87e52ae36e3ca0874d606a329013b462b60c
fernando henrique <fernandomk6@gmail.com>
2022-06-29 10:01:49 -0300

    refatoração primaria exercicio cronometro

--- 7-exercicios/cronometro/index.js
+++ 7-exercicios/cronometro/index.js
@@ -1,43 +1,45 @@
-// setInterval(e => console.log(new Date(time+=1000)), .3);
 const startBtn = document.querySelector('#start-btn');
 const stopBtn = document.querySelector('#stop-btn');
 const clearBtn = document.querySelector('#clear-btn');
 const timer = document.querySelector('#timer span');
-const initialDate = new Date();
-initialDate.setHours(0);
-initialDate.setMinutes(0);
-initialDate.setSeconds(0);
-initialDate.setMilliseconds(0);
-let intervalTimer = 0;
 
 let secondsPassed = 0;
-function renderTime(initialDate) {
+let intervalTimer = 0;
+
+function getInitialDate() {
+  const initialDate = new Date();
+  initialDate.setHours(0);
+  initialDate.setMinutes(0);
+  initialDate.setSeconds(0);
+  initialDate.setMilliseconds(0);
+  return initialDate;
+}
+
+function renderTime() {
+  const initialDate = getInitialDate();
   const timeFormated = new Date(initialDate.getTime() + secondsPassed).toLocaleTimeString('pt-BR');
+
   secondsPassed += 1000;
   timer.innerHTML = timeFormated;
 }
 
 function startTimer() {
-  
-  intervalTimer = setInterval(e => {
-    renderTime(initialDate);
-  }, 0.1);
-  
-  stopTimer(intervalTimer);
-  clearTimer();
+  startBtn.setAttribute('disabled', true);
+  intervalTimer = setInterval(renderTime, 1);
+  console.log(intervalTimer);
 }
 
-function stopTimer(intervalTimer) {
-  stopBtn.addEventListener('click', e => {
-    clearInterval(intervalTimer);
-  });
+function stopTimer() {
+  startBtn.removeAttribute('disabled');
+  console.log(intervalTimer);
+  clearInterval(intervalTimer);
 }
 
 function clearTimer() {
-  clearBtn.addEventListener('click', e => {
-    secondsPassed = 0;
-    renderTime(initialDate);
-  });
+  secondsPassed = 0;
+  renderTime();
 }
 
 startBtn.addEventListener('click', startTimer);
+stopBtn.addEventListener('click', stopTimer);
+clearBtn.addEventListener('click', clearTimer);
