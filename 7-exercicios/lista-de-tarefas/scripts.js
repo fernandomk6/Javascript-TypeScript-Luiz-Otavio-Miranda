f9c3a74f1c7a3ab3c061a4b0a113d3ef7b6da6b6
fernando henrique <fernandomk6@gmail.com>
2022-06-29 16:42:14 -0300

    refatorado exercicio lista de tarefas

--- 7-exercicios/lista-de-tarefas/scripts.js
+++ 7-exercicios/lista-de-tarefas/scripts.js
@@ -1,10 +1,22 @@
+// global declarations
 const input = document.querySelector('.list-input-container input');
 const addButton = document.querySelector('.list-input-container button');
 const taskList = document.querySelector('.list-content');
 
+// functions
+function saveTask() {
+  const tasks = [];
+  for (const task of taskList.querySelectorAll('li')) {
+    tasks.push(task.innerText.replace('Excluir', '').trim()); 
+  }
+    
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 function buildTask(taskText) {
   const li = document.createElement('li');
   const button = document.createElement('button');
+  button.dataset.script = 'delete-btn';
 
   li.innerHTML = taskText;
   button.innerHTML = 'Excluir';
@@ -14,22 +26,40 @@ function buildTask(taskText) {
 }
 
 function addtask () {
+  if (!input.value) return;
+
   const taskText = input.value;
   const taskElement = buildTask(taskText);
 
   input.value = '';
-  taskList.appendChild(taskElement);
-
-  loadDeleteTask();
+  taskList.prepend(taskElement);
+  input.focus();
+  saveTask();
 }
 
-function loadDeleteTask() {
-  const deleteButtons = document.querySelectorAll('.list-content button');
-  deleteButtons.forEach(button => {
-    button.addEventListener('click', event => {
-      event.target.parentElement.remove();
-    });
-  });
+function loadTasks() {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  if (tasks) {
+    for (const task of tasks) {
+      taskList.appendChild(buildTask(task));
+    }
+  }
 }
+
+loadTasks();
+
+// events
+input.addEventListener('keypress', event => {
+  if (event.key === 'Enter') {
+    addtask();
+  }
+});
 
 addButton.addEventListener('click', addtask);
+
+document.addEventListener('click', event => {
+  if (event.target.dataset.script === 'delete-btn') {
+    event.target.parentElement.remove();
+    saveTask();
+  }
+});
